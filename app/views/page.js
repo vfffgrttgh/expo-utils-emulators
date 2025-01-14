aa6eb08df2ab7ce8cec43b10998012646395b8f3
vfffgrttgh <163544061+vfffgrttgh@users.noreply.github.com>
2025-01-14 18:40:11 +0530

    Update page.js

--- app/views/page.js
+++ app/views/page.js
@@ -2,8 +2,8 @@ import { Suspense } from "react";
 
 export default function Page() {
     const data = [
-        { id: 1, src: "https://appetize.io/embed/6q5hgbmupyc6c5mhznjd37goga?device=pixel7&amp;launchUrl=exp%3A%2F%2Fu.expo.dev%2F933fd9c0-1666-11e7-afca-d980795c5824%3Fruntime-version%3Dexposdk%253A51.0.0%26channel-name%3Dproduction%26snack-channel%3DlwfOGBik8t&amp;params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&amp;appearance=light&amp;deviceColor=black&amp;scale=auto&amp;orientation=portrait&amp;centered=both" },
-        { id: 2, src: "https://appetize.io/embed/7kfu7r2ugwm7c4risjyigiuhve?device=iphone16pro&launchUrl=exp%3A%2F%2Fu.expo.dev%2F933fd9c0-1666-11e7-afca-d980795c5824%3Fruntime-version%3Dexposdk%253A51.0.0%26channel-name%3Dproduction%26snack-channel%3DtQEbahbfwX&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&appearance=light&deviceColor=black&scale=auto&orientation=portrait&centered=both" },
+        { id: 1, src: "https://appetize.io/embed/6q5hgbmupyc6c5mhznjd37goga?device=pixel7" },
+        { id: 2, src: "https://appetize.io/embed/7kfu7r2ugwm7c4risjyigiuhve?device=iphone16pro" },
     ];
 
     return (
